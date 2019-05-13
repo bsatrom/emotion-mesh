bd80ac0169a0cdf93f8ae1d324715ef3fdbdea62
Brandon Satrom <bsatrom@gmail.com>
2019-05-13 13:28:27 -0500

    add bounding box and save local file; closes #4

--- cloud-detector/detect-face.py
+++ cloud-detector/detect-face.py
@@ -3,12 +3,18 @@ import json
 import os
 import config
 import cognitive_face as CF
+from io import BytesIO
+from PIL import Image, ImageDraw
 
 subscription_key = config.face_api_key
 assert subscription_key
 
 face_api_url = config.face_api_url
 assert face_api_url
+
+image_base_path = 'cloud-detector/resources/'
+image_name = 'streamers'
+image_path = image_base_path + image_name + '.png'
 
 CF.Key.set(subscription_key)
 CF.BaseUrl.set(face_api_url)
@@ -18,7 +24,32 @@ attrs = 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlus
 # faces = CF.face.detect('cloud-detector/resources/Dagestani_man_and_woman.jpg',
 #                       face_id=True, landmarks=False, attributes=attrs)
 
-faces = CF.face.detect('cloud-detector/resources/streamers.png',
-                       face_id=True, landmarks=False, attributes=attrs)
+faces = CF.face.detect(image_path, face_id=True,
+                       landmarks=False, attributes=attrs)
 
 print(faces)
+
+# Convert width height to a point in a rectangle
+
+
+def getRectangle(faceDictionary):
+    rect = faceDictionary['faceRectangle']
+    left = rect['left']
+    top = rect['top']
+    bottom = left + rect['height']
+    right = top + rect['width']
+    return ((left, top), (bottom, right))
+
+
+# Download the image from the url
+img = Image.open(image_path)
+
+# For each face returned use the face rectangle and draw a red box.
+draw = ImageDraw.Draw(img)
+for face in faces:
+    # draw a rectangle on each face
+    draw.rectangle(getRectangle(face), outline='red')
+
+# Display the image in the users default image browser.
+img.show()
+img.save(image_base_path + image_name + "_modified.png", "PNG")
