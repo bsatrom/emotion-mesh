ec172de9d14d1ac0214e90e1aeec0de04b10839c
Brandon Satrom <bsatrom@gmail.com>
2019-05-13 14:07:37 -0500

    added emotion text to image; closes #4

--- cloud-detector/detect-face.py
+++ cloud-detector/detect-face.py
@@ -2,9 +2,10 @@ import requests
 import json
 import os
 import config
+import operator
 import cognitive_face as CF
 from io import BytesIO
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 subscription_key = config.face_api_key
 assert subscription_key
@@ -27,9 +28,14 @@ attrs = 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlus
 faces = CF.face.detect(image_path, face_id=True,
                        landmarks=False, attributes=attrs)
 
-print(faces)
-
 # Convert width height to a point in a rectangle
+
+
+def getCoordsForText(faceDictionary):
+    rect = faceDictionary['faceRectangle']
+    left = rect['left']
+    top = rect['top']
+    return (left, top - 10)
 
 
 def getRectangle(faceDictionary):
@@ -41,7 +47,13 @@ def getRectangle(faceDictionary):
     return ((left, top), (bottom, right))
 
 
-# Download the image from the url
+def getMainEmotion(faceDictionary):
+    # Get the emotion collection and sort in acending order to get the top result
+    emotions = faceDictionary['faceAttributes']['emotion']
+    emotions = sorted(emotions, key=emotions.get, reverse=True)
+    return emotions[0]
+
+
 img = Image.open(image_path)
 
 # For each face returned use the face rectangle and draw a red box.
@@ -49,7 +61,12 @@ draw = ImageDraw.Draw(img)
 for face in faces:
     # draw a rectangle on each face
     draw.rectangle(getRectangle(face), outline='red')
+    # write the predominant emotion over the bounding box
+    draw.text(getCoordsForText(face), getMainEmotion(
+        face), fill=(255, 255, 255, 255))
 
 # Display the image in the users default image browser.
 img.show()
 img.save(image_base_path + image_name + "_modified.png", "PNG")
+
+# print(faces)
