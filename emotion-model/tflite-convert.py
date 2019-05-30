ad3d93eb4fcc902c69b1ec3a610f466036f0398b
Brandon Satrom <bsatrom@gmail.com>
2019-05-30 16:18:12 -0500

    WIP: build quantized models

--- emotion-model/tflite-convert.py
+++ emotion-model/tflite-convert.py
@@ -2,9 +2,11 @@ import tensorflow as tf
 
 print(tf.__version__)
 
+file = "models/_mini_XCEPTION.01-0.40-quantized"
+
 converter = tf.lite.TFLiteConverter.from_keras_model_file(
-    "models/_mini_XCEPTION.134-0.66.hdf5")
-# converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
-converter.post_training_quantize = True
+    file + ".hdf5")
+
+# converter.post_training_quantize = True
 tflite_model = converter.convert()
-open("models/_mini_XCEPTION.134-0.66-optimized.tflite", "wb").write(tflite_model)
+open(file + "-optimized.tflite", "wb").write(tflite_model)
