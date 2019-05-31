5adfc9efa32f609f4a0cfd370ec72b49a0020643
Brandon Satrom <brandon@particle.io>
2019-05-31 19:53:54 +0000

    WIP on quantized model

---
+++ local-detector/local-emotion/predict.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import imutils
+import cv2
+import numpy as np
+import sys
+import time
+
+detection_model_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
+emotion_model_path = 'model/_mini_XCEPTION.134-0.66.hdf5'
+
+def predict_emotion(img_path):
+  start = time.time()
+
+  # hyper-parameters for bounding boxes shape
+  # loading models
+  face_detection = cv2.CascadeClassifier(detection_model_path)
+  emotion_classifier = load_model(emotion_model_path, compile=False)
+  EMOTIONS = ["angry", "disgust", "scared",
+              "happy", "sad", "surprised", "neutral"]
+
+  # reading the frame
+  orig_frame = cv2.imread(img_path)
+  frame = cv2.imread(img_path, 0)
+  faces = face_detection.detectMultiScale(
+      frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+  if len(faces) > 0:
+      faces = sorted(faces, reverse=True, key=lambda x: (
+          x[2] - x[0]) * (x[3] - x[1]))[0]
+      (fX, fY, fW, fH) = faces
+      roi = frame[fY:fY + fH, fX:fX + fW]
+      roi = cv2.resize(roi, (48, 48))
+      roi = roi.astype("float") / 255.0
+      roi = img_to_array(roi)
+      roi = np.expand_dims(roi, axis=0)
+      preds = emotion_classifier.predict(roi)[0]
+
+      emotion_probability = np.max(preds)
+      label = EMOTIONS[preds.argmax()]
+      cv2.putText(orig_frame, label, (fX, fY - 10),
+                  cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+      cv2.rectangle(orig_frame, (fX, fY), (fX + fW, fY + fH), (0, 0, 255), 2)
+
+  cv2.imwrite(img_path.split('.')[0]+'_prediction.png', orig_frame)
+
+  end = time.time()
+
+  print("Execution Time: " + str(end-start))
